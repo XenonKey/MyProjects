3f929586f0abc42d56ca5c61212b2e821645380b
giorgi <g.suleimanashvili7@gmail.com>
2025-12-09 15:45:53 +0400

    add new func

--- libs2/my_math.py
+++ libs2/my_math.py
@@ -6,5 +6,12 @@ def multiply(a: int, b) -> int:
     return a * b
 
 
+<<<<<<< Updated upstream
 def mult_by_10(a) -> int:
     return a * 10
+=======
+def add_my_mail(mail):
+    return f'This is my mail {mail}'
+
+print(add_my_mail('sds'))
+>>>>>>> Stashed changes
